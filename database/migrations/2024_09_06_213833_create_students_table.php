4d5f81c325d5eb73ddd3ca707ba20ad483c522e2
kent dayola <kentdayola@gmail.com>
2024-09-06 14:44:49 -0700

    Innitial Commit for activity-3

--- database/migrations/2024_09_06_213833_create_students_table.php
+++ database/migrations/2024_09_06_213833_create_students_table.php
@@ -11,11 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('student', function (Blueprint $table) {
+        Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
+            $table->integer('age');
             $table->timestamps();
         });
     }
@@ -25,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('student');
+        Schema::dropIfExists('students');
     }
 };
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Models\student;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +15,36 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/student/create', function(){
+    $student = new student();
+    $student->first_name = 'John';
+    $student->last_name = 'Doe';
+    $student->email = 'john.doe@example.com';
+    $student->age = 22;
+    $student->save();
+    return 'Student Created!';       
+}
+);
+
+Route::get('/student', function(){
+    $student = student::all();
+    return $student;
+}
+);
+
+Route::get('/student/update', function(){
+    $student = student::where('email', 'john.doe@example.com')->first();
+    $student->email = 'john.doe@example.com';
+    $student->age = 23;
+    $student->save();
+    return 'Students Updated!!';
+}
+);
+
+Route::get('/student/delete', function(){
+    $student = student::where('email', 'john.doe@example.com')->first();
+    $student->delete();
+    return 'Student Deleted!';
 });
